75b26e955b5491ca457b79fb09d668a7f7baf746
shtya <shtya54@gmail.com>
2025-09-23 08:25:34 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,26 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { LoggingValidationPipe } from 'common/translationPipe';
 import { ConfigService } from '@nestjs/config';
 import { QueryFailedErrorFilter } from 'common/QueryFailedErrorFilter';
- 
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3030;
   // Get the ConfigService instance
   const configService = app.get(ConfigService);
 
-  
   app.useGlobalFilters(app.get(QueryFailedErrorFilter));
   app.useStaticAssets(join(__dirname, '..', '..', '/uploads'), { prefix: '/uploads/' });
-
+  const allowedOrigins = ['http://localhost:3000', 'https://fitness-front-iin2.vercel.app'];
   app.enableCors({
-    origin: configService.get('ALLOWED_ORIGINS')?.split(',') || '*', // Allow specific origins or all
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // للسماح بـ Postman وما شابه
+      if (allowedOrigins.includes(origin)) return callback(null, true);
+      return callback(new Error(`CORS blocked for origin: ${origin}`), false);
+    },
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
-    allowedHeaders: 'Content-Type,Authorization,x-lang',
-    exposedHeaders: 'Content-Length,Content-Range',
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-lang'],
+    exposedHeaders: ['Content-Length', 'Content-Range'],
   });
 
   app.setGlobalPrefix('api/v1');
@@ -30,7 +33,7 @@ async function bootstrap() {
   const loggingValidationPipe = app.get(LoggingValidationPipe);
   app.useGlobalPipes(loggingValidationPipe);
 
-  app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false , transform : true , forbidNonWhitelisted : true , whitelist : true }));
+  app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false, transform: true, forbidNonWhitelisted: true, whitelist: true }));
 
   Logger.log(`🚀 server is running on port ${port}`);
 
